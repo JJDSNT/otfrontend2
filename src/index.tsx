840bae6d1b196d3d3be00f19dca5f228930784da
Jaime Dias <jaimejosediasnt@gmail.com>
2022-04-22 04:08:32 -0300

    Burger menu

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends React.Component {
-
-    render() {
-        return <div>Hi there!</div>
-    }
-
-}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
