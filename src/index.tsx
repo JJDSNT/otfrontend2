8cb1af9224fb63ffc4ad09953df39dd3b640192c
Jaime Dias <jaimejosediasnt@gmail.com>
2023-01-31 01:17:55 -0300

    revertendo o createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-// @ts-ignore
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 
 
